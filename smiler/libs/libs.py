c843f2076180b322f9ecc47744f272516023556c
Aleksandr Pilgun <alexandr.pilgun@gmail.com>
2020-10-12 16:53:45 +0200

    new apktool

--- smiler/libs/libs.py
+++ smiler/libs/libs.py
@@ -4,4 +4,4 @@ import sys
 from pkg_resources import resource_filename
 
 class Libs:
-    APKTOOL_PATH = resource_filename("smiler.libs.jars", "apktool_2.3.1.jar")
+    APKTOOL_PATH = resource_filename("smiler.libs.jars", "apktool_2.4.1.jar")
