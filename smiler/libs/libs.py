957dbce84cbd5495d0f805e52297c940b9068daf
Michael <auermich@fim.uni-passau.de>
2023-09-28 12:58:39 +0200

    update apktool to version 2.8.1

--- smiler/libs/libs.py
+++ smiler/libs/libs.py
@@ -4,4 +4,4 @@ import sys
 from pkg_resources import resource_filename
 
 class Libs:
-    APKTOOL_PATH = resource_filename("smiler.libs.jars", "apktool_2.4.1.jar")
+    APKTOOL_PATH = resource_filename("smiler.libs.jars", "apktool_2.8.1.jar")
